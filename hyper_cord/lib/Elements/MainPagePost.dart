b4a8a41942e8a3ae194262f4a253d7c128a612d1
Yakir Oren <yakiroren@gmail.com>
2020-04-12 16:42:30 +0300

    add GestureDetector to the MainPagePost class

--- hyper_cord/lib/Elements/MainPagePost.dart
+++ hyper_cord/lib/Elements/MainPagePost.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hyper_cord/Pages/MainPage.dart';
 
 class MainPagePost extends StatefulWidget {
   final String date;
@@ -18,41 +19,43 @@ class _MainPagePostState extends State<MainPagePost> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Container(
-        child: Stack(
-          children: <Widget>[
-            Container(
-              
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(25.0),
-                child: Image.asset(
-                  widget.imagePath,
-                  height: 200.0,
-                  fit: BoxFit.fill,
+      child: GestureDetector(
+        onTap: (){},
+        child: Container(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(25.0),
+                  child: Image.asset(
+                    widget.imagePath,
+                    height: 200.0,
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              child: SafeArea(
+              Positioned(
+                child: SafeArea(
+                  child: Text(
+                    widget.title,
+                    textDirection: TextDirection.rtl,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                bottom: 15,
+                right: 10,
+              ),
+              Positioned(
                 child: Text(
-                  widget.title,
+                  widget.date + "-" + widget.category,
                   textDirection: TextDirection.rtl,
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  style: TextStyle(color: Colors.white, fontSize: 12),
                 ),
-              ),
-              bottom: 15,
-              right: 10,
-            ),
-            Positioned(
-              child: Text(
-                widget.date + "-" + widget.category,
-                textDirection: TextDirection.rtl,
-                style: TextStyle(color: Colors.white, fontSize: 12),
-              ),
-              bottom: 5,
-              right: 10,
-            )
-          ],
+                bottom: 5,
+                right: 10,
+              )
+            ],
+          ),
         ),
       ),
     );
