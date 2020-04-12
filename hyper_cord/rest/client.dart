2587749986c7ec584c8fce13abdfc54e58d5f05c
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-12 15:14:11 +0300

    finished client v1 (?)

--- hyper_cord/rest/client.dart
+++ hyper_cord/rest/client.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 import 'package:yaml/yaml.dart';
 import 'package:http/http.dart' as http;
+import './utils.dart';
 
-const BASE_URL =  "hypercord.co.il/api";
+const BASE_URL =  "https://hypercord.co.il/api";
 
 Function getMember(http.Client object, String name) {
    if (name == "GET") return object.get;
@@ -18,12 +19,28 @@ class ApiClient {
   * Sends off a request to hypercord.co.il's API 
   * with a given method, endpoint and body if specified.
   */
-  void request(String method, String endpoint, [String body]) async {
-    try {
-      var uriResponse = await (getMember(client, method)(BASE_URL + endpoint, body: body));
-    } catch (exception) {
-      print(exception);
+  Future<http.Response> request(String method, String endpoint, [String body]) async {
+    Map<String, String> headers = {
+      "XF-Api-Key": this.superUserKey,
+      "XF-Api-User": "1",
+      "Content-Type": "application/x-www-form-urlencoded"
+    };
+
+    if (method == "GET") {
+      return await (client.get(BASE_URL + endpoint, headers: headers));
     }
+    
+    if (method == "POST") {
+      return client.post(BASE_URL + endpoint, body: body, headers: headers);
+    }
+  }
+
+  Future<http.Response> get(String endpoint) {
+    return request("GET", endpoint);
+  }
+
+  Future<http.Response> post(String endpoint, Map<String,String> body) {
+    return request("POST", endpoint, getQueryString(body));
   }
 
   ApiClient() {
